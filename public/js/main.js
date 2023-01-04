2492484d9f005bf8052622af35015f84cf201595
Shahin-Alz <shaalemzadeh@gmail.com>
2023-01-04 10:29:33 +0100

    ajout

---
+++ public/js/main.js
@@ -0,0 +1,72 @@
+//exo1
+
+// let shahin = {
+//     nom : 'Alemzadeh',
+//     prenom : 'Shahin',
+//     age : 22,
+//     taille : "1m73",
+
+
+
+// }
+
+// console.log(shahin.taille);
+
+//exo2
+
+// let benja = {
+//     nom : 'Btn',
+//     prenom : 'Benja',
+//     age : 22,
+//     taille : "1m73",
+
+//     sepresenter(){
+
+//     }
+
+// }
+
+//exo3
+// let gia = {
+//     nom : shahin.nom,
+//     prenom : benja.prenom,
+//     age : 24,
+//     taille : "1m85",
+
+// }
+
+// console.log(gia);
+
+//exo4
+
+// let mathieu = {
+
+//     prenom: "mathieu",
+//     nom: "bil",
+//     sePresenter() {
+//         console.log(`je m'appel ${mathieu.prenom} ${mathieu.nom}`);
+//     }
+
+// }
+
+// mathieu.sePresenter()
+
+
+
+
+//exo5
+//pas finis
+
+// let objt = {
+//     nom: "alem",
+
+//     actiond() {
+//         let age = +prompt("entrer un age")
+//         push.objt(age)
+//     }
+
+// }
+
+// objt.actiond()
+
+// alert(`${objt.nom} a ${objt.age} ans`)
